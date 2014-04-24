a3da4260f243adf5a0f85c68990f34b5a64114d8
Cullen Tsering <cullen.tsering@live.com>
2014-04-23 20:25:41 -0500

    initial commit

---
+++ src/SolutionVersion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("Dear Diary is an application that demonstrates full stack .net solution accessible from multiple types of devices")]
+[assembly: AssemblyCompany("D. Cullen Tsering")]
+[assembly: AssemblyProduct("Dear.Diary")]
+[assembly: AssemblyCopyright("Copyright © D. Cullen Tsering 2014")]
+[assembly: AssemblyTrademark("Dear.Diary")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
+
+[assembly: AssemblyInformationalVersion("0.1.0.0")]
+[assembly: ComVisibleAttribute(false)]
+[assembly: CLSCompliant(true)]
