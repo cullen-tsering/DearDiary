39ddeefcb3a183676e11ff6044fc291f5830d718
Cullen Tsering <cullen.tsering@live.com>
2014-04-23 20:39:35 -0500

    added the diary entry domain model

---
+++ src/DearDiary.Core/DomainModels/DiaryEntry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DearDiary.Core.DomainModels
+{
+    public class DiaryEntry
+    {
+        //A unique id
+        public Guid Id { get; set; }
+        /// <summary>
+        /// The date when the diary entry was created
+        /// </summary>
+        public DateTime DateTime { get; set; }
+
+        //the name of the author
+        public string Author { get; set; }
+
+        //the location where diary was written
+        public string Location { get; set; }
+
+        //the content of the diary
+        public string Content { get; set; }
+    }
+}
