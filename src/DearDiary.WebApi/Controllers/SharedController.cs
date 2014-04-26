2ffb08dfd0ceae1bc643173085d75419028e7cfd
Cullen Tsering <cullen.tsering@live.com>
2014-04-26 17:27:29 -0500

    added shared controller

---
+++ src/DearDiary.WebApi/Controllers/SharedController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace DearDiary.WebApi.Controllers
+{
+    /// <summary>
+    /// this controller will be shared by all the other API controller 
+    /// so code can be shared by all child controllers
+    /// </summary>
+    public class SharedController : ApiController
+    {
+    }
+}
